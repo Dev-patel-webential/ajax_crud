89f110e55d75328604d46da250ab06ebc0d87fe4
DESKTOP-1R4GV4O\admin <dev.webential@gmail.com>
2023-03-03 16:48:31 -0800

    my text

--- test.php
+++ test.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-<<<<<<< HEAD
+
     <title>Hello, world!</title>
     <style type="text/css">
     	footer {
@@ -152,7 +152,7 @@ div.social {
 
 
     <footer>
-    	
+    <h1>Rupesh this is my line</h1>	
     	<div class="row primary">
     <div class="column about">
         <h3>Connect</h3>
